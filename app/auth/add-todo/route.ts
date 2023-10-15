cdf549144f3662dc066e154e2517964eeb64b845
Treimart <treier@tlu.ee>
2023-10-15 19:35:19 +0300

    Added an input field for authenthicated users for inserting their own todos

---
+++ app/auth/add-todo/route.ts
@@ -0,0 +1,41 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import { NextResponse } from "next/server"
+const { v4: uuidv4 } = require("uuid")
+uuidv4() // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+
+export const dynamic = "force-dynamic"
+
+export async function POST(request: Request) {
+  const requestUrl = new URL(request.url)
+  const formData = await request.formData()
+  const title = String(formData.get("title"))
+  const supabase = createRouteHandlerClient({ cookies })
+
+  const { error } = await supabase.from("todos").insert([
+    {
+      id: uuidv4(),
+      created_at: new Date().toISOString(),
+      title: title,
+      is_complete: false,
+      user_id: null
+    }
+  ])
+
+  if (error) {
+    console.log("Error inserting data: ", error)
+    return NextResponse.redirect(
+      `${requestUrl.origin}/todo?error=Could not add todo`,
+      {
+        status: 301
+      }
+    )
+  }
+
+  return NextResponse.redirect(
+    `${requestUrl.origin}/todo?message=Todo has been added`,
+    {
+      status: 301
+    }
+  )
+}
--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,5 +1,6 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
+import { Box, Button, TextField } from "@mui/material"
 
 export const dynamic = "force-dynamic"
 
@@ -8,5 +9,42 @@ export default async function ServerComponent() {
 
   const { data: todos } = await supabase.from("todos").select()
 
-  return <pre>{JSON.stringify(todos, null, 2)}</pre>
+  return (
+    <div>
+      <pre>{JSON.stringify(todos, null, 2)}</pre>
+
+      <form
+        action="/auth/add-todo"
+        method="post"
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            gap: "2em"
+          }}
+        >
+          <TextField
+            required
+            name="title"
+            id="title"
+            label="Title"
+            variant="outlined"
+            placeholder="Take dog for a walk"
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+          >
+            Add todo
+          </Button>
+        </Box>
+      </form>
+    </div>
+  )
 }
