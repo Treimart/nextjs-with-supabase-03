0eff0c7e7bff0e849800bd83f5ddef4486f7238b
Treimart <treier@tlu.ee>
2023-10-15 18:17:43 +0300

    Converted buttons to MUI

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,29 +5,9 @@ import { Box, Button, TextField } from "@mui/material"
 export default function Login() {
   return (
     <div>
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{" "}
-        Back
-      </Link>
+      <Button href="/">Back</Button>
 
       <form
-        className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
         action="/auth/sign-in"
         method="post"
       >
@@ -68,6 +48,12 @@ export default function Login() {
             variant="contained"
           >
             Sign in
+          </Button>
+          <Button
+            formAction="/auth/sign-up"
+            type="submit"
+          >
+            Sign Up
           </Button>
         </Box>
         <Messages />
