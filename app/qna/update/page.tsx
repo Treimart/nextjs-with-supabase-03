8a2268282b69491cddf281f807b1bd6df08f79be
Treimart <treier@tlu.ee>
2023-10-27 20:26:20 +0300

    Add Update component for qna

---
+++ app/qna/update/page.tsx
@@ -0,0 +1,61 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import { Box, Button, TextField } from "@mui/material"
+
+export const dynamic = "force-dynamic"
+
+export default async function ServerComponent() {
+  const supabase = createServerComponentClient({ cookies })
+
+  const { data: qnas } = await supabase.from("qnas").select()
+
+  return (
+    <div>
+      <pre>{JSON.stringify(qnas, null, 2)}</pre>
+
+      <form
+        action="/auth/update-qna"
+        method="post"
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            gap: "2em"
+          }}
+        >
+          <TextField
+            required
+            name="id"
+            id="id"
+            label="ID"
+            variant="outlined"
+            placeholder="410fb1a2-c7b8-4174-a1b5-ef1ab5568687"
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+          <TextField
+            required
+            name="anwser"
+            id="anwser"
+            label="Anwser"
+            variant="outlined"
+            placeholder="An octopus has eigth legs."
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+          >
+            Update qna
+          </Button>
+        </Box>
+      </form>
+    </div>
+  )
+}
